74cf16ed89090d7c9e425ad3fcc702e9462794e5
Petar <kanev_petar@yahoo.com>
2022-11-25 01:11:43 +0200

    ng build firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBQZOXD75Vb5A057OSp5CPChl6Q1zQFK2Q',
+    authDomain: 'ng-fitness-tracker-f4c26.firebaseapp.com',
+    // databaseURL: '<your-database-URL>',
+    projectId: 'ng-fitness-tracker-f4c26',
+    storageBucket: 'ng-fitness-tracker-f4c26.appspot.com',
+    messagingSenderId: '662842066902',
+    appId: '1:662842066902:web:17563ac172135774fc8677',
+    // measurementId: '<your-measurement-id>'
+  }
 };
